633f4a4fb8f86bc6dab287489689c84be372b8de
willianchip <52382869+willianchip@users.noreply.github.com>
2025-12-09 21:57:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 import express from "express";
-import sessionRoutes from "./src/routes/sessionRoutes.js";
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/session", sessionRoutes);
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-import express from "express";
-import sessionRoutes from "./routes/sessionRoutes.js"; // <-- CORRETÍSSIMO
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
 app.use(express.json());
