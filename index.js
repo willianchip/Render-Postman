c88e94cc855f8a6f906df9cf57701150b7cf3922
willianchip <52382869+willianchip@users.noreply.github.com>
2025-12-09 21:13:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import express from "express";
-import cors from "cors";
-import sessionRoutes from "./src/routes/sessionRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
-// base route
-app.get("/", (req, res) => res.send("Evo API - OK"));
-
-// mount
 app.use("/api/session", sessionRoutes);
 
 const PORT = process.env.PORT || 10000;
