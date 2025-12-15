1c80be0699b12d7af008a002fa5170a91afd967a
willianchip <52382869+willianchip@users.noreply.github.com>
2025-12-14 22:59:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import express from "express";
-import cors from "cors";
 
-// CORREÇÃO AQUI: Apontamos para o arquivo REAL que você tem no GitHub
-import sessionRoutes from "./src/routes/api.js"; 
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// A rota continua sendo /api/session
-app.use("/api/session", sessionRoutes);
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
