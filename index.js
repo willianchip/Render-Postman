d46da8e21c1acb654789d95faf3ab55ecec0fd09
willianchip <52382869+willianchip@users.noreply.github.com>
2025-12-23 03:44:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-import "./src/server.js";
+import app from "./src/server.js";
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor rodando na porta", PORT);
+});
