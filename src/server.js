30e8ca0cf0734cd9d07d97597b68d2fe1a315f3a
willianchip <52382869+willianchip@users.noreply.github.com>
2025-12-23 03:43:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import { createSession, getQR } from './sessions.js';
+import express from "express";
+import { sessionRoutes } from "./sessions.js";
 
 const app = express();
 app.use(express.json());
 
-app.post('/sessions', createSession);
-app.get('/sessions/:id/qr', getQR);
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log('Servidor rodando na porta', PORT);
+app.get("/", (_, res) => {
+  res.json({ status: "API online" });
 });
+
+sessionRoutes(app);
+
+export default app;
